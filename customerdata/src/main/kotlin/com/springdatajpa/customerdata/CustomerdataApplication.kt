bde90138877c1d45398fdb271453cbee504137c5
Elias-Isaac Phiri <phirielias@gmail.com>
2018-08-15 10:03:51 +0200

    Initial commit

---
+++ customerdata/src/main/kotlin/com/springdatajpa/customerdata/CustomerdataApplication.kt
@@ -0,0 +1,11 @@
+package com.springdatajpa.customerdata
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CustomerdataApplication
+
+fun main(args: Array<String>) {
+    runApplication<CustomerdataApplication>(*args)
+}
